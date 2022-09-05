65b13f6dc31b333f9a212dfdba3ccacae8239dbe
Parker Parham <parkerparham@gmail.com>
2022-09-05 12:20:41 -0400

    commit tree 7c2201eb2af9756bc4720d1e5ab50dd2a66926d7

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [itemStatus, toggleStatus] = useState(false)
+
+  function toggleStatusClick(){
+    toggleStatus(!itemStatus)
+  }
+
+  const itemClass = itemStatus ? "in-cart" : "";
+
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={toggleStatusClick} className="add">{itemStatus ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, toggleCategory] = useState("All")
+
+  function toggleCategoryChange(event) {
+    toggleCategory(event.target.value)
+  }
+
+const itemsToDisplay = items.filter((item) => {
+  if (selectedCategory === "All"){
+    return true;
+  } else {
+    return item.category === selectedCategory
+  }
+})  
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={toggleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +27,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
